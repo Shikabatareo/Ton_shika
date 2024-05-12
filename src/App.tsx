bf8957ba938a1e0c2395e44f49a65e41e8bcfff1
Nikita Holopov <Shikabatareo@gmail.com>
2024-05-12 10:26:33 +0300

    asdadqweqweq

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,71 @@ import './App.css'
 
 import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react'
 
+import { useTonAddress, useTonWallet } from '@tonconnect/ui-react'
+
+import { Locales, useTonConnectUI } from '@tonconnect/ui-react'
+
+// export const EntrypointPage = () => {
+// 	const connectionRestored = useIsConnectionRestored()
+
+// 	if (!connectionRestored) {
+// 		return <Loader>Please wait...</Loader>
+// 	}
+
+// 	return <MainPage />
+// }
+
+export const Settings = () => {
+	const [tonConnectUI, setOptions] = useTonConnectUI()
+
+	const onLanguageChange = (lang: string) => {
+		setOptions({ language: lang as Locales })
+	}
+
+	return (
+		<div>
+			<button onClick={() => tonConnectUI.sendTransaction(myTransaction)}>
+				Send transaction
+			</button>
+
+			<div>
+				<label>language</label>
+				<select onChange={e => onLanguageChange(e.target.value)}>
+					<option value='en'>en</option>
+					<option value='ru'>ru</option>
+				</select>
+			</div>
+		</div>
+	)
+}
+
+export const Wallet = () => {
+	const wallet = useTonWallet()
+
+	return (
+		wallet && (
+			<div>
+				<span>Connected wallet: {wallet.name}</span>
+				<span>Device: {wallet.device.appName}</span>
+			</div>
+		)
+	)
+}
+
+export const Address = () => {
+	const userFriendlyAddress = useTonAddress()
+	const rawAddress = useTonAddress(false)
+
+	return (
+		userFriendlyAddress && (
+			<div>
+				<span>User-friendly address: {userFriendlyAddress}</span>
+				<span>Raw address: {rawAddress}</span>
+			</div>
+		)
+	)
+}
+
 export const Header = () => {
 	return (
 		<header>
@@ -19,6 +84,9 @@ function App() {
 		<TonConnectUIProvider manifestUrl='https://shikabatareo.github.io/ton_shika/tonconnect-manifest.json'>
 			<div>
 				<Header />
+				<Address />
+				<Wallet />
+				<Settings />
 			</div>
 		</TonConnectUIProvider>
 	)
